b18dbb32621e4a170987859042af8e66d2511458
Gustavo0123 <2190716@my.ipleiria.pt>
2020-05-12 09:02:27 +0000

    Instruçoes

---
+++ weblogicmvc/webapp/model/tabuleiro.php
@@ -0,0 +1,35 @@
+<?php
+
+
+class tabuleiro
+{
+    private $dado;
+
+    public function resultadoDado1(){
+
+}
+    public function resultadoDado2(){
+
+}
+    public function numerosBloqueioP1(){
+
+}
+    public function numerosBloqueioP2(){
+
+}
+    public function rolarDados(){
+
+}
+    public function checkFinalJogadaP1(){
+
+}
+    public function checkFinalJogadaP2(){
+
+}
+    public function getVencedor(){
+
+}
+    public function getPointsVencedor(){
+
+}
+}

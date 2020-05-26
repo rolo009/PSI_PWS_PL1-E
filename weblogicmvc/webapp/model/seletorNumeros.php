e3aa2134a5ca5361c9ef9daf28d4e4ab5b8247c1
rolo009 <pedrorolo@live.com.pt>
2020-05-26 10:03:19 +0100

    123

--- weblogicmvc/webapp/model/seletorNumeros.php
+++ weblogicmvc/webapp/model/seletorNumeros.php
@@ -4,4 +4,24 @@
 class seletorNumeros
 {
 
+
+    public function validateNumber(){
+
+    }
+
+    public function selection(){
+
+    }
+
+    public function checkSelectionTotal(){
+
+    }
+
+    public function clearSelection(){
+
+    }
+
+    public function selectionHasNumber(){
+
+    }
 }
